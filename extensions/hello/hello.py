3a89e561b94e37c0892b4ceec7cb1d740d22c3ed
Serhii Sliptchenko <sliptchenko71@gmail.com>
2024-05-03 21:34:41 +0300

    * rm hello extension

--- extensions/hello/hello.py
+++
@@ -1,9 +0,0 @@
-from core.extension import KairyoExtension
-from extensions.hello.widgets.hello import Hello
-
-
-class HelloExtension(KairyoExtension):
-    """Simple Hello World! extension"""
-
-    def on_start(self):
-        self.api.user_interface.register_tab("hello", Hello())
