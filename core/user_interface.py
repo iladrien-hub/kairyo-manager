aea25f33db5d8f4b29a2c8320835bb3cb0b5c7f3
Serhii Sliptchenko <sliptchenko71@gmail.com>
2024-05-05 14:35:29 +0300

    *(register_tab): add icon option

--- core/user_interface.py
+++ core/user_interface.py
@@ -1,5 +1,7 @@
 import typing
 
+from PyQt5 import QtWidgets, QtGui
+
 if typing.TYPE_CHECKING:
     from mainwindow import MainWindow
 
@@ -8,5 +10,7 @@ class UserInterface:
     def __init__(self, window: 'MainWindow'):
         self._window = window
 
-    def register_tab(self, name, widget):
-        self._window.tabs.addTab(widget, name)
+    def register_tab(self, name: str, widget: QtWidgets.QWidget, icon: QtGui.QIcon = None):
+        tab_idx = self._window.tabs.addTab(widget, name)
+        if icon:
+            self._window.tabs.setTabIcon(tab_idx, icon)
