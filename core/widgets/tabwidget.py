0df0232f7eff55ef2302e6784be0b85d94106f06
Serhii Sliptchenko <sliptchenko71@gmail.com>
2024-05-07 14:54:14 +0300

    *(TabWidget): general refactoring

--- core/widgets/tabwidget.py
+++ core/widgets/tabwidget.py
@@ -64,33 +64,31 @@ class TabWidget(QtWidgets.QWidget):
         super().__init__()
 
         self._tabs: List[OrientedButton] = []
-        self._tabs_rotation: OrientedButton.Orientation = OrientedButton.Orientation.Normal
+        self._tabsRotation: OrientedButton.Orientation = OrientedButton.Orientation.Normal
 
-        self._always_open: bool = False
-        self._stack_widget = QtWidgets.QStackedWidget()
+        self._alwaysOpen: bool = False
+        self._stackWidget = QtWidgets.QStackedWidget()
 
-        self._tabs_layout = QtWidgets.QHBoxLayout()
-        self._tabs_widget = QtWidgets.QWidget()
+        self._tabsLayout = QtWidgets.QHBoxLayout()
+        self._tabsWidget = QtWidgets.QWidget()
         self._layout = QtWidgets.QBoxLayout(QtWidgets.QBoxLayout.Direction.Up)
 
         self.setTabPosition(QtWidgets.QTabWidget.North)
         self.setupUi()
 
-    # noinspection PyPep8Naming
     def setupUi(self):
-        self._stack_widget.setVisible(False)
-        # self._stack_widget.setStyleSheet(self._STYLE_TAB_WINDOW)
+        # self._stackWidget.setVisible(False)
 
-        self._tabs_layout.setContentsMargins(1, 1, 1, 1)
-        self._tabs_layout.setSpacing(0)
+        self._tabsLayout.setContentsMargins(1, 1, 1, 1)
+        self._tabsLayout.setSpacing(0)
 
         spacer = QtWidgets.QSpacerItem(1, 1, QSizePolicy.Expanding, QSizePolicy.Minimum)
-        self._tabs_layout.addItem(spacer)
+        self._tabsLayout.addItem(spacer)
 
-        self._tabs_widget.setLayout(self._tabs_layout)
+        self._tabsWidget.setLayout(self._tabsLayout)
 
-        self._layout.addWidget(self._tabs_widget)
-        self._layout.addWidget(self._stack_widget)
+        self._layout.addWidget(self._tabsWidget)
+        self._layout.addWidget(self._stackWidget)
         self._layout.setContentsMargins(0, 0, 0, 0)
         self._layout.setSpacing(0)
 
@@ -99,42 +97,40 @@ class TabWidget(QtWidgets.QWidget):
     # noinspection PyPep8Naming
     def addTab(self, w: QtWidgets.QWidget, name: str):
         # button = QtWidgets.QPushButton(name)
-        button = OrientedButton(name, self, orientation=self._tabs_rotation)
+        button = OrientedButton(name, self, orientation=self._tabsRotation)
         button.setText(name)
         button.setCheckable(True)
         button.setSizePolicy(QSizePolicy.Minimum, QSizePolicy.Minimum)
         button.setObjectName("tabButton")
 
-        self._tabs_layout.insertWidget(self._tabs_layout.count() - 1, button)
+        self._tabsLayout.insertWidget(self._tabsLayout.count() - 1, button)
         self._tabs.append(button)
 
-        self._stack_widget.addWidget(w)
+        self._stackWidget.addWidget(w)
 
         button.clicked.connect(partial(self.onTabClicked, button))  # noqa
         self.updateState()
 
         return self._tabs.index(button)
 
-    # noinspection PyPep8Naming
     def updateState(self):
         checked = next(filter(lambda x: x.isChecked(), self._tabs), None)
         if checked is None:
-            if self._always_open and self._tabs:
+            if self._alwaysOpen and self._tabs:
                 checked = self._tabs[0]
                 checked.setChecked(True)
             else:
-                self._stack_widget.setVisible(False)
+                self._stackWidget.setVisible(False)
                 return
 
-        self._stack_widget.setVisible(True)
+        self._stackWidget.setVisible(True)
         index = self._tabs.index(checked)
-        self._stack_widget.setCurrentIndex(index)
-        self._stack_widget.setSizePolicy(QSizePolicy.Expanding, QSizePolicy.Expanding)
+        self._stackWidget.setCurrentIndex(index)
+        self._stackWidget.setSizePolicy(QSizePolicy.Expanding, QSizePolicy.Expanding)
 
-    # noinspection PyPep8Naming
     def onTabClicked(self, b: QtWidgets.QPushButton):
         try:
-            if self._always_open and not b.isChecked():
+            if self._alwaysOpen and not b.isChecked():
                 b.setChecked(True)
                 return
 
@@ -146,32 +142,29 @@ class TabWidget(QtWidgets.QWidget):
         except BaseException as e:
             print(e)
 
-    # noinspection PyPep8Naming
     def setTabPosition(self, a0: QtWidgets.QTabWidget.TabPosition):
-        layout_direction, tab_direction, self._tabs_rotation = self._TAB_POSITION_PARAMS[a0]
+        layout_direction, tab_direction, self._tabsRotation = self._TAB_POSITION_PARAMS[a0]
 
-        for i in range(self._tabs_layout.count()):
-            item = self._tabs_layout.itemAt(i)
+        for i in range(self._tabsLayout.count()):
+            item = self._tabsLayout.itemAt(i)
             if isinstance(item, QtWidgets.QSpacerItem):
-                self._tabs_layout.removeItem(item)
+                self._tabsLayout.removeItem(item)
                 spacer = QtWidgets.QSpacerItem(1, 1, *self._SPACER_ORIENTATION[a0])
-                self._tabs_layout.insertItem(i, spacer)
+                self._tabsLayout.insertItem(i, spacer)
 
         self._layout.setDirection(layout_direction)
-        self._tabs_layout.setDirection(tab_direction)
+        self._tabsLayout.setDirection(tab_direction)
         for b in self._tabs:
-            b.setOrientation(self._tabs_rotation)
+            b.setOrientation(self._tabsRotation)
 
-        self._tabs_widget.setObjectName(self._TABS_WIDGET_STYLES[a0])
-        self._tabs_layout.setContentsMargins(*self._TABS_WIDGET_CONTENT_MARGINS[a0])
+        self._tabsWidget.setObjectName(self._TABS_WIDGET_STYLES[a0])
+        self._tabsLayout.setContentsMargins(*self._TABS_WIDGET_CONTENT_MARGINS[a0])
 
-    # noinspection PyPep8Naming
     def setAlwaysOpen(self, b: bool):
-        self._always_open = b
-        if self._always_open:
+        self._alwaysOpen = b
+        if self._alwaysOpen:
             self.updateState()
 
-    # noinspection PyPep8Naming
     def setTabIcon(self, index: int, icon: QtGui.QIcon):
         btn = self._tabs[index]
         btn.setIcon(icon)
