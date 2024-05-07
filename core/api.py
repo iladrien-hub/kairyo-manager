ef6de7661990ec8463e18a7a4710f538b7c45b10
Serhii Sliptchenko <sliptchenko71@gmail.com>
2024-05-07 17:47:57 +0300

    *(api): +settings

--- core/api.py
+++ core/api.py
@@ -1,5 +1,7 @@
 import typing
 
+from PyQt5.QtCore import QSettings, Qt
+
 if typing.TYPE_CHECKING:
     from .extension import KairyoExtension
     from .user_interface import UserInterface
@@ -13,6 +15,7 @@ class KairyoApi:
         self.__extensions: typing.List['KairyoExtension'] = []
         self.__user_interface: 'UserInterface' = user_interface
         self.__storage: 'KairyoStorage' = KairyoStorage()
+        self.__settings: QSettings = QSettings('config.ini', QSettings.IniFormat)
 
     def register_extension(self, ext: 'KairyoExtension'):
         self.__extensions.append(ext)
@@ -24,3 +27,7 @@ class KairyoApi:
     @property
     def user_interface(self):
         return self.__user_interface
+
+    @property
+    def settings(self):
+        return self.__settings
