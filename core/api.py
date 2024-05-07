3bec91121ab38e1670c842e61f727fe230a55e31
Serhii Sliptchenko <sliptchenko71@gmail.com>
2024-05-07 18:32:09 +0300

    *(api): make singleton

--- core/api.py
+++ core/api.py
@@ -10,8 +10,13 @@ from .storage import KairyoStorage
 
 
 class KairyoApi:
+    __instance = None
 
     def __init__(self, *, user_interface: 'UserInterface'):
+        if KairyoApi.__instance:
+            raise RuntimeError(f"only one instance of {self.__class__.__name__} can be created")
+        KairyoApi.__instance = self
+
         self.__extensions: typing.List['KairyoExtension'] = []
         self.__user_interface: 'UserInterface' = user_interface
         self.__storage: 'KairyoStorage' = KairyoStorage()
@@ -19,6 +24,10 @@ class KairyoApi:
 
     def register_extension(self, ext: 'KairyoExtension'):
         self.__extensions.append(ext)
+
+    @classmethod
+    def instance(cls) -> 'KairyoApi':
+        return cls.__instance
 
     @property
     def extensions(self):
