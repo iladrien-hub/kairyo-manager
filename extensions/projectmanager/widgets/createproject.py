dbc5e42f7d5551c1043fc9766eebabd91cd7b83f
Serhii Sliptchenko <sliptchenko71@gmail.com>
2024-05-19 16:13:58 +0300

    *(projectmanager): move project settings to separate widget

--- extensions/projectmanager/widgets/createproject.py
+++ extensions/projectmanager/widgets/createproject.py
@@ -3,13 +3,13 @@ import os.path
 from typing import Optional
 
 from PyQt5 import QtWidgets
-from PyQt5.QtWidgets import QSizePolicy
 
 from core.api import KairyoApi
 from core.styling.icon import load_icon
 from core.widgets.fileinput import FileInput, FileInputMode
 from core.widgets.forms import LabeledDivider
-from core.widgets.layout import create_box_layout, create_grid_layout, layout_to_widget
+from core.widgets.layout import create_box_layout, layout_to_widget
+from .projectinfo import ProjectInfo
 
 
 @dataclasses.dataclass
@@ -38,42 +38,11 @@ class CreateProject(QtWidgets.QWidget):
         self._location.setMode(FileInputMode.Directory)
         self._location.textChanged.connect(self.checkForErrors)
 
+        self._info = ProjectInfo()
+
         self._createButton = QtWidgets.QPushButton('Create')
         self._createButton.setProperty('accent', True)
         self._createButton.clicked.connect(self.on_createButton_clicked)
-
-        self._sourceTypeRadio_default = QtWidgets.QRadioButton('Source Type:')
-        self._sourceTypeRadio_default.setChecked(True)
-
-        self._sourceTypeRadio_custom = QtWidgets.QRadioButton('Custom Source:')
-
-        self._sourceTypeRadioGroup = QtWidgets.QButtonGroup()
-        self._sourceTypeRadioGroup.addButton(self._sourceTypeRadio_default)
-        self._sourceTypeRadioGroup.addButton(self._sourceTypeRadio_custom)
-        self._sourceTypeRadioGroup.setExclusive(True)
-        self._sourceTypeRadioGroup.buttonClicked.connect(self.updateFranchiseControls)
-
-        self._sourceType = QtWidgets.QComboBox(self)
-        self._sourceType.setItemDelegate(QtWidgets.QStyledItemDelegate(self))
-        self._sourceType.addItems(['Anime', 'Game', 'Franchise'])
-        self._sourceType.setSizePolicy(QSizePolicy.Expanding, QSizePolicy.Maximum)
-
-        self._sourceTypeCustom = QtWidgets.QLineEdit()
-        self._sourceTypeCustom.setSizePolicy(QSizePolicy.Expanding, QSizePolicy.Maximum)
-
-        self._characterName = QtWidgets.QLineEdit()
-        self._characterName.setPlaceholderText('E.g. Sakura Haruno')
-        self._loadCharacterFromLora = QtWidgets.QCheckBox("Use character from LoRa")
-        self._loadCharacterFromLora.stateChanged.connect(self.updateCharacterNameInput)
-        self._loadCharacterFromLora.setVisible(False)
-
-        self._characterNameLabel = QtWidgets.QLabel('Character Name:')
-
-        self._sourceName = QtWidgets.QLineEdit()
-        self._sourceName.setPlaceholderText("E.g. Naruto: Shippūden")
-
-        self._description = QtWidgets.QTextEdit()
-        self._description.setPlaceholderText('write something...')
 
         self._errorLabel = QtWidgets.QLabel()
         self._errorIcon = QtWidgets.QLabel()
@@ -90,23 +59,8 @@ class CreateProject(QtWidgets.QWidget):
             ], proto=QtWidgets.QHBoxLayout, spacing=10),
             LabeledDivider('Details'),
             create_box_layout([
-                create_grid_layout([
-                    [self._characterNameLabel],
-                    [self._characterName, self._loadCharacterFromLora]
-                ], spacing=6),
-                create_grid_layout([
-                    [self._sourceTypeRadio_default, self._sourceType],
-                    [self._sourceTypeRadio_custom, self._sourceTypeCustom]
-                ], spacing=10),
-                create_box_layout([
-                    QtWidgets.QLabel("Source Name:"),
-                    self._sourceName
-                ], proto=QtWidgets.QHBoxLayout, spacing=10),
-                create_box_layout([
-                    QtWidgets.QLabel("Description:"),
-                    self._description
-                ], spacing=6),
-            ], spacing=12, margins=(16, 0, 0, 0))
+                self._info
+            ], margins=(16, 0, 0, 0))
         ], spacing=12, margins=(12, 12, 12, 12))
 
         layout.addStretch()
@@ -121,18 +75,8 @@ class CreateProject(QtWidgets.QWidget):
         self.setMinimumHeight(624)
         self.setMinimumWidth(680)
 
-        self.updateFranchiseControls()
-        self.updateCharacterNameInput()
         self.initLocation()
         self.checkForErrors()
-
-    def updateFranchiseControls(self):
-        self._sourceType.setEnabled(self._sourceTypeRadio_default.isChecked())
-        self._sourceTypeCustom.setEnabled(self._sourceTypeRadio_custom.isChecked())
-
-    def updateCharacterNameInput(self):
-        self._characterName.setEnabled(not self._loadCharacterFromLora.isChecked())
-        self._characterNameLabel.setEnabled(not self._loadCharacterFromLora.isChecked())
 
     def initLocation(self):
         self._location.setFocus()
@@ -168,12 +112,12 @@ class CreateProject(QtWidgets.QWidget):
     def on_createButton_clicked(self):
         self._result = CreateProjectResult(
             location=self._location.text(),
-            character=self._characterName.text(),
-            description=self._description.toPlainText(),
-            source=self._sourceName.text(),
-            source_type=self._sourceType.currentText(),
-            custom_source_type=self._sourceTypeCustom.text(),
-            use_custom_source_type=self._sourceTypeRadio_custom.isChecked(),
-            use_character_from_lora=self._loadCharacterFromLora.isChecked()
+            character=self._info.character(),
+            description=self._info.description(),
+            source=self._info.source(),
+            source_type=self._info.sourceType(),
+            custom_source_type=self._info.customSourceType(),
+            use_custom_source_type=self._info.useCustomSourceType(),
+            use_character_from_lora=self._info.useCharacterFromLora()
         )
         self.window().close()
---
+++ extensions/projectmanager/widgets/projectinfo.py
@@ -0,0 +1,94 @@
+from PyQt5 import QtWidgets
+from PyQt5.QtWidgets import QSizePolicy
+
+from core.widgets.layout import create_box_layout, create_grid_layout
+
+
+class ProjectInfo(QtWidgets.QFrame):
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        self._sourceTypeRadio_default = QtWidgets.QRadioButton('Source Type:')
+        self._sourceTypeRadio_default.setChecked(True)
+
+        self._sourceTypeRadio_custom = QtWidgets.QRadioButton('Custom Source:')
+
+        self._sourceTypeRadioGroup = QtWidgets.QButtonGroup()
+        self._sourceTypeRadioGroup.addButton(self._sourceTypeRadio_default)
+        self._sourceTypeRadioGroup.addButton(self._sourceTypeRadio_custom)
+        self._sourceTypeRadioGroup.setExclusive(True)
+        self._sourceTypeRadioGroup.buttonClicked.connect(self.updateFranchiseControls)
+
+        self._sourceType = QtWidgets.QComboBox(self)
+        self._sourceType.setItemDelegate(QtWidgets.QStyledItemDelegate(self))
+        self._sourceType.addItems(['Anime', 'Game', 'Franchise'])
+        self._sourceType.setSizePolicy(QSizePolicy.Expanding, QSizePolicy.Maximum)
+
+        self._sourceTypeCustom = QtWidgets.QLineEdit()
+        self._sourceTypeCustom.setSizePolicy(QSizePolicy.Expanding, QSizePolicy.Maximum)
+
+        self._characterName = QtWidgets.QLineEdit()
+        self._characterName.setPlaceholderText('E.g. Sakura Haruno')
+        self._loadCharacterFromLora = QtWidgets.QCheckBox("Use character from LoRa")
+        self._loadCharacterFromLora.stateChanged.connect(self.updateCharacterNameInput)
+        self._loadCharacterFromLora.setVisible(False)
+
+        self._characterNameLabel = QtWidgets.QLabel('Character Name:')
+
+        self._sourceName = QtWidgets.QLineEdit()
+        self._sourceName.setPlaceholderText("E.g. Naruto: Shippūden")
+
+        self._description = QtWidgets.QTextEdit()
+        self._description.setPlaceholderText('write something...')
+
+        self.setLayout(create_box_layout([
+            create_grid_layout([
+                [self._characterNameLabel],
+                [self._characterName, self._loadCharacterFromLora]
+            ], spacing=6),
+            create_grid_layout([
+                [self._sourceTypeRadio_default, self._sourceType],
+                [self._sourceTypeRadio_custom, self._sourceTypeCustom]
+            ], spacing=10),
+            create_box_layout([
+                QtWidgets.QLabel("Source Name:"),
+                self._sourceName
+            ], proto=QtWidgets.QHBoxLayout, spacing=10),
+            create_box_layout([
+                QtWidgets.QLabel("Description:"),
+                self._description
+            ], spacing=6),
+        ], spacing=12, margins=(0, 0, 0, 0)))
+
+        self.updateFranchiseControls()
+        self.updateCharacterNameInput()
+
+    def updateFranchiseControls(self):
+        self._sourceType.setEnabled(self._sourceTypeRadio_default.isChecked())
+        self._sourceTypeCustom.setEnabled(self._sourceTypeRadio_custom.isChecked())
+
+    def updateCharacterNameInput(self):
+        self._characterName.setEnabled(not self._loadCharacterFromLora.isChecked())
+        self._characterNameLabel.setEnabled(not self._loadCharacterFromLora.isChecked())
+
+    def character(self):
+        return self._characterName.text()
+
+    def description(self):
+        return self._description.toPlainText()
+
+    def source(self):
+        return self._sourceName.text()
+
+    def sourceType(self):
+        return self._sourceType.currentText()
+
+    def customSourceType(self):
+        return self._sourceTypeCustom.text()
+
+    def useCustomSourceType(self):
+        return self._sourceTypeRadio_custom.isChecked()
+
+    def useCharacterFromLora(self):
+        return self._loadCharacterFromLora.isChecked()
